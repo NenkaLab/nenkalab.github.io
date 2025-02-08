75f0d150150c025b5ba67cfb81945f0657ef55ae
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-02-08 14:22:26 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	let { children } = $props();
+	export const prerender = true;
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
