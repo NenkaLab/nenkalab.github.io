be968b7a537a8fea571a7b3f2c24ab2c5b0bcefa
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-02-09 18:04:50 +0900

    Commit
    외출복귀 하아..

--- src/routes/utils/base64/+page.svelte
+++ src/routes/utils/base64/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { MdOutlinedTextField, MdTabs } from "@material/web/all";
 	import { onMount } from "svelte";
 
 
@@ -6,11 +7,51 @@
         history.back();
     }
 
+
+    let tab: MdTabs;
+
+    let encodePage: HTMLElement;
+    let encodeInput: MdOutlinedTextField;
+    let encodeOutput: MdOutlinedTextField;
+
+    let decodePage: HTMLElement;
+    let decodeInput: MdOutlinedTextField;
+    let decodeOutput: MdOutlinedTextField;
+
+    function b64EncodeUnicode(str: string): string {
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+            return String.fromCharCode(parseInt(p1, 16))
+        }))
+    }
+
+    function b64DecodeUnicode(str: string): string {
+        return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        }).join(''))
+    }
+
+
+    function changeTab(event: any) {
+        if (event.target.activeTabIndex == 0) {
+            encodePage.classList.remove('hide');
+            decodePage.classList.add('hide');
+        } else {
+            encodePage.classList.add('hide');
+            decodePage.classList.remove('hide');
+        }
+    }
+
     onMount(() => {
         setTimeout(() => {
             // @ts-ignore
             document.querySelector("#b-tabs").shadowRoot.querySelector("md-divider")?.remove();
         }, 200);
+
+        tab.addEventListener('change', changeTab);
+
+        return () => {
+            tab.removeEventListener('change', changeTab);
+        }
     });
 </script>
 
@@ -49,6 +90,10 @@ md-tabs {
     }
 }
 
+.hide {
+    display: none;
+}
+
 </style>
 
 <h1 class="b-title">
@@ -59,26 +104,51 @@ md-tabs {
     Base64
 </h1>
 
-<md-tabs id="b-tabs">
+<md-tabs id="b-tabs" bind:this={tab}>
     <md-primary-tab active>Encode</md-primary-tab>
     <md-primary-tab>Decode</md-primary-tab>
 </md-tabs>
 
-<section id="encode">
+<section id="encode" bind:this={encodePage}>
     <div>
         <md-outlined-text-field
             id="encode-input"
-            label="Base64 를 입력하세요"
-            type="textarea">
+            label="텍스트를 입력하세요"
+            type="textarea"
+            style="width: 300px;height: 200px;"
+            bind:this={encodeInput}
+            oninput="{() => {
+                encodeOutput.value = b64EncodeUnicode(encodeInput.value);
+            }}">
         </md-outlined-text-field>
         <md-outlined-text-field
             id="encode-output"
             label="출력"
-            type="textarea">
+            type="textarea"
+            style="width: 300px;height: 200px;"
+            bind:this={encodeOutput}>
         </md-outlined-text-field>
     </div>
 </section>
 
-<section id="decode">
-
+<section id="decode" class="hide" bind:this={decodePage}>
+    <div>
+        <md-outlined-text-field
+            id="decode-input"
+            label="Base64 를 입력하세요"
+            type="textarea"
+            style="width: 300px;height: 200px;"
+            bind:this={decodeInput}
+            oninput="{() => {
+                decodeOutput.value = b64DecodeUnicode(decodeInput.value);
+            }}">
+        </md-outlined-text-field>
+        <md-outlined-text-field
+            id="decode-output"
+            label="출력"
+            type="textarea"
+            style="width: 300px;height: 200px;"
+            bind:this={decodeOutput}>
+        </md-outlined-text-field>
+    </div>
 </section>
