09652f0354c48cd162f389a0c733d8f31f2b2107
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-10-21 20:07:15 +0900

    feat: Tailwind CSS 설정에 다크 모드 및 커스텀 변형 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-//   darkMode: 'selector',
+  darkMode: 'selector',
   content: [
     './_includes/**/*.{html,js}',
     './_layouts/**/*.{html,js}',
