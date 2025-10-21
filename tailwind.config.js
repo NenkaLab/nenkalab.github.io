d8d7e11a07719d4f5f8b2d0152d8150ea424d471
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-10-21 19:33:41 +0900

    feat: Tailwind CSS 통합 및 Jekyll 배포 워크플로우 설정

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.md',
+    './*.html',
+    './*.md'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
+}
