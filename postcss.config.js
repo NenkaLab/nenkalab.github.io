e2bf1a2476e87f1b1f76c83fe4284f4cb9bfccfc
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-10-22 06:31:41 +0900

    test 10220631

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
     '@tailwindcss/postcss': {},
     autoprefixer: {},
     cssnano: process.env.NODE_ENV === 'production' ? { preset: 'default' } : false,
