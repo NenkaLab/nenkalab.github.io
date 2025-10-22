9f3f4ee725a050a0b8c6b394a8eadb9fbab894d6
NenkaLab <29904002+NenkaLab@users.noreply.github.com>
2025-10-22 19:22:35 +0900

    test 10221922

--- _plugins/last_modified.rb
+++ _plugins/last_modified.rb
@@ -12,14 +12,6 @@ module Jekyll
         # 파일 시스템의 수정 시간 가져오기
         post.data['last_modified_at'] = File.mtime(post.path)
       end
-      
-      site.utils.docs.each do |util|
-        # front matter에 이미 있으면 건너뛰기
-        next if util.data['last_modified_at']
-        
-        # 파일 시스템의 수정 시간 가져오기
-        util.data['last_modified_at'] = File.mtime(util.path)
-      end
     end
   end
 end
